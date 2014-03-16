6df09f7bdc3d89611d2c1086d80f53cddc64eb30
Peter Battaglia <petersbattaglia@hotmail.com>
2014-03-15 23:13:42 -0700

    Initial Commit

---
+++ LoggerApp/MainWindow.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LoggerApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private ObservableCollection<string> _Log = new ObservableCollection<string>();
+
+        public ObservableCollection<string> Log
+        {
+            get
+            {
+                return _Log;
+            }
+        }
+        
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Worker.WorkDone += Worker_WorkDone;
+            Worker.WorkCompleted += Worker_WorkCompleted;
+        }
+
+        void Worker_WorkCompleted(object sender, WorkerEventArgs e)
+        {
+            MessageBox.Show("Work Completed.");
+        }
+
+        void Worker_WorkDone(object sender, WorkerEventArgs e)
+        {
+            Dispatcher.BeginInvoke(
+                new Action(() => 
+                { 
+                    Log.Insert(0, e.Message); 
+                }));
+        }
+
+        private void btnStart_Click(object sender, RoutedEventArgs e)
+        {
+            new Thread(() => { Worker.DoWork(); }).Start();
+        }
+    }
+}
---
+++ LoggerApp/Worker.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace LoggerApp
+{
+    public static class Worker
+    {
+        public delegate void WorkDoneEventHandler(object sender, WorkerEventArgs e);
+        public delegate void WorkCompletedEventHandler(object sender, WorkerEventArgs e);
+
+        public static event WorkDoneEventHandler WorkDone;
+        public static event WorkCompletedEventHandler WorkCompleted;
+
+        public static void DoWork()
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                Thread.Sleep(2000);
+                OnWorkDone(new WorkerEventArgs(string.Format("Iteration #{0} Complete.", (i + 1).ToString())));
+            }
+
+            OnWorkCompleted(new WorkerEventArgs("All done."));
+        }
+
+        public static void OnWorkDone(WorkerEventArgs e)
+        {
+            if (WorkDone != null)
+                WorkDone(null, e);
+        }
+
+        public static void OnWorkCompleted(WorkerEventArgs e)
+        {
+            if (WorkCompleted != null)
+                WorkCompleted(null, e);
+        }
+    }
+
+    public class WorkerEventArgs : EventArgs
+    {
+        public WorkerEventArgs(string message)
+        {
+            Message = message;
+        }
+
+        public string Message { get; private set; }
+    }
+}
